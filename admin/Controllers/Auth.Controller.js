2286076cc006623af319015909a0435ee46c7e1e
gauravsharmajsi <gauravsharmajsi@gmail.com>
2024-04-10 13:21:03 +0530

    Fixed Admin Login apibug

--- admin/Controllers/Auth.Controller.js
+++ admin/Controllers/Auth.Controller.js
@@ -125,7 +125,7 @@ module.exports = {
 
     res.status(200)
     .send({
-      message: "Login Otp sended successfully"+otp,
+      message: "Login Otp sended successfully",
       mobile: mobile,
     });
 
